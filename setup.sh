9eaf9eccaba7785fb862ecc6a72a192e1a67e87e
erl-ang <ell2147@columbia.edu>
2023-03-27 05:06:53 +0000

    setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,32 @@
+# Install all dependencies for the project
+sudo apt update
+sudo apt-get update
+sudo apt install python3-pip
+pip3 install -U pip setuptools wheel
+pip3 install beautifulsoup4 
+pip3 install google-api-python-client
+pip3 install openAI
+pip3 install prettytables
+pip3 install pytorch-pretrained-bert
+pip3 install -U spacy
+pip3 install torch
 
+python3 -m spacy download en_core_web_lg
+
+git clone https://github.com/zackhuiiiii/SpanBERT/
+cd SpanBERT
+# TODO: make sure this works
+pip3 install -r requirements.txt
+bash download_finetuned.sh
+
+# move back to repo's root dir
+cd ..
+mv lib ./SpanBERT/lib
+
+# Moving files into correct dir structure for running:
 # File directory setup. These files need to be in the
 # same directory as the SpanBERT directory.
-mv EntityExtractor.py ./SpanBERT/EntityExtractor.py
-mv project2.py ./SpanBERT/project2.py
-mv QueryExecutor.py ./SpanBERT/QueryExecutor.py
-
-mv lib ./SpanBERT/lib
+mv GPT3Extractor.py ./SpanBERT
+mv project2.py ./SpanBERT
+mv QueryExecutor.py ./SpanBERT
+mv SpanBertExtractor.py ./SpanBERT
