31acae61f57e20dfa9e5863e21ce12c70052db64
erl-ang <ell2147@columbia.edu>
2023-03-27 03:13:40 +0000

    setup the dir structure to run spanbert correctly

---
+++ setup.sh
@@ -0,0 +1,8 @@
+
+# File directory setup. These files need to be in the
+# same directory as the SpanBERT directory.
+mv EntityExtractor.py ./SpanBERT/EntityExtractor.py
+mv project2.py ./SpanBERT/project2.py
+mv QueryExecutor.py ./SpanBERT/QueryExecutor.py
+
+mv lib ./SpanBERT/lib
