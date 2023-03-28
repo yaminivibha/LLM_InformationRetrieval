9f573c232076591e4e460f13171aea3b516b30c7
Yamini on VM <yva2002@columbia.edu>
2023-03-28 02:29:14 +0000

    move stuff works now

--- move_stuff.sh
+++ move_stuff.sh
@@ -1,22 +1,19 @@
-# Call this after git pulling 
-# When dir structure has SpanBert but things have updated outside
-
-# Note - call from inside llm_ise
-
-rm ./SpanBERT/QueryExecutor.py
-rm ./SpanBERT/GPT3Extractor.py
-rm ./SpanBERT/SpanBertExtractor.py
-rm ./SpanBERT/project2.py
-
-
+if test -f "QueryExecutor.py"; 
+then rm ./SpanBERT/QueryExecutor.py
 mv QueryExecutor.py ./SpanBERT/QueryExecutor.py
+fi
+
+if test -f "GPT3Extractor.py"; 
+then rm ./SpanBERT/GPT3Extractor.py
 mv GPT3Extractor.py ./SpanBERT/GPT3Extractor.py
+fi
+
+if test -f "SpanBertExtractor.py"; 
+then rm ./SpanBERT/SpanBertExtractor.py
 mv SpanBertExtractor.py ./SpanBERT/SpanBertExtractor.py
+fi
+
+if test -f "project2.py"; 
+then rm ./SpanBERT/project2.py  
 mv project2.py ./SpanBERT/project2.py
-
-
-
-
-
-
-
+fi
